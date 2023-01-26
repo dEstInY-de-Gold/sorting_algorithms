b6acd4df46920a2864bede1878784ea613134714
dEstInY-de-Gold <noahayebu@gmail.com>
2023-01-26 01:08:46 +0000

    debuged selection sort file

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -17,7 +17,7 @@ void swap_ints(int *a, int *b)
 
 /**
  * selection_sort - Sort an array of integers in ascending order
- *                  using the selection sort algorithm.
+ * using the selection sort algorithm.
  * @array: An array of integers.
  * @size: The size of the array.
  *
